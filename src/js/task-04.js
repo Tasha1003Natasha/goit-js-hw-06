8005e28cf76ddc239776a38189562faafe7757a6
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-14 19:27:29 +0300

    9 задач

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -1,9 +1,3 @@
-// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника та ініціалізуй її значенням 0.
-// Додай слухачів кліків до кнопок, всередині яких збільшуй або зменшуй значення лічильника.
-// Оновлюй інтерфейс новим значенням змінної counterValue.
-
-
-
 const buttonDecrement = document.querySelector(`button[data-action="decrement"]`);  
 const buttonIncrement = document.querySelector(`button[data-action="increment"]`);
 let counterValue = 0;
--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodyRef = document.querySelector("body")
+const buttonRef = document.querySelector(".change-color");
+const backgroundRef = document.querySelector(".color");
+
+const handleClick = () => {
+  const color = getRandomHexColor();
+  bodyRef.style.background = color;
+  backgroundRef.textContent = color;
+};
+
+buttonRef.addEventListener("click", handleClick);
+
+
