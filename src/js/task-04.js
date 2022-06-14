3b0e2f44b1aa655df3ff37286b27dab5dcb28aaa
Tasha1003Natasha <0955712824n@gmail.com>
2022-06-14 16:25:20 +0300

    Задача 4 готова

--- src/js/task-04.js
+++ src/js/task-04.js
@@ -4,23 +4,22 @@
 
 
 
-const buttonDecrement = document.querySelector(`button[data-action="decrement"]`);
+const buttonDecrement = document.querySelector(`button[data-action="decrement"]`);  
 console.log(buttonDecrement);
 const buttonIncrement = document.querySelector(`button[data-action="increment"]`);
 console.log(buttonIncrement);
 let counterValue = 0;
 
 buttonDecrement.addEventListener("click", handlerDecrement);
-buttonIncrement.removeEventListener("click", handlerIncrement); 
+buttonIncrement.addEventListener("click", handlerIncrement); 
 
-function handlerClick(event) {
-  console.log(event.target);
-  const target = event.target;
+
+function handlerDecrement() {
+  counterValue = 0;
+  document.getElementById("value").innerHTML = --counterValue;
 }
 
-// function reset(event) {
-//   console.log(event.target);
-//   counterValue -= 1; 
-//   }
-
+function handlerIncrement() {
+  document.getElementById("value").innerHTML = ++counterValue;
+  }
 
